7899a9791fb45166f26626a3a2ec5bc52c65c1a6
Cheng <ryanchang9422@gmail.com>
2025-07-30 00:30:28 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,76 @@
+from bson.objectid import ObjectId
+import pymongo
+client =pymongo.MongoClient("mongodb+srv://ryan:ryan1234@mycluster.rf6tx9u.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster")
+db=client.goldbuysell
+collection=db.gold
+print("資料庫連線成功")
+
+
+from flask import *
+app=Flask(__name__)
+app.secret_key="1234"
+
+from datetime import datetime
+
+#首頁
+@app.route("/")
+def home():
+    #平均買入價格
+    def clt_avg_amount(transactions):
+        buy_transactions=[t for t in transactions if t["type"]=="buy"]
+        if not buy_transactions:
+            return 0
+        total_amount=sum([t['amount'] for t in transactions])
+        total_weight=sum([t['weight'] for t in transactions])
+        return total_amount/total_weight if total_weight!=0 else 0
+       
+    
+    records=list(collection.find().sort("timestamp",-1))
+
+    stats={
+        "total_buy":sum(t["amount"] for t in records if t["type"]=="buy"),
+        "total_amount":sum(t["amount"] for t in records if t["type"]=="buy"),
+        "total_avg_amount":clt_avg_amount(records),
+        "total_weight":sum(t['weight'] for t in records if t["type"]=="buy")
+    }   
+    return render_template("home.html",records=records,stats=stats)
+
+#新增
+@app.route("/add",methods=["GET","POST"])
+def add():
+    if request.method=="POST":
+        timestamp_str=request.form.get("timestamp")
+        if timestamp_str:
+            timestamp=datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M")
+        else:
+            timestamp=datetime.now()
+        transaction={
+            "type":request.form.get("type"),
+            "weight":float(request.form.get("weight")),
+            "price":float(request.form.get("price")),
+            "amount":float(request.form.get("weight"))*float(request.form.get("price")),
+            "note":request.form.get("note"),
+            "timestamp":timestamp                                       
+        }
+        collection.insert_one(transaction)
+        return redirect("/")
+    return render_template("add.html")
+
+#刪除
+@app.route("/delete/<id>",methods=["POST"])
+def delete(id):
+    collection.delete_one({"_id":ObjectId(id)})
+    return redirect("/")
+
+#篩選日期
+@app.route("/choosedate")
+def choosedate():
+    
+    
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
+
